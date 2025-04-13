8a23f477deecd32896ace05934e8a95e9945780f
saud <saudmalik619@gmail.com>
2025-04-13 21:11:06 +0500

    feat: bank details api

--- routes/api.php
+++ routes/api.php
@@ -36,4 +36,5 @@ Route::prefix('v1')->group(function () {
     require base_path('routes/api-group/cart/cart.php');
     require base_path('routes/api-group/cart/checkout.php');
     require base_path('routes/api-group/conversation/conversations.php');
+    require base_path('routes/api-group/user/bank.php');
 });

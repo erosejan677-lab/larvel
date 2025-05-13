c1872f8c5500d4c406de316ca9ae503594f706d5
saud <saudmalik619@gmail.com>
2025-05-14 01:00:10 +0500

    fix: google route get->post

--- routes/api-group/auth/social-auth.php
+++ routes/api-group/auth/social-auth.php
@@ -5,4 +5,4 @@ use App\Http\Controllers\Api\V1\Auth\GoogleAuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/auth/google/redirect/url', [GoogleAuthController::class, 'redirectToGoogle']);
-Route::get('/auth/google/callback', [GoogleAuthController::class, 'handleGoogleCallback']);
+Route::post('/auth/google/callback', [GoogleAuthController::class, 'handleGoogleCallback']);

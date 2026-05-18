82493b95b205dc2062b6444155fc482a98b948b7
ukhan1122 <143578110+ukhan1122@users.noreply.github.com>
2026-05-18 17:19:24 +0500

    Update 2025_07_16_110021_create_guest_cart_items_table.php
    repalced

--- database/migrations/2025_07_16_110021_create_guest_cart_items_table.php
+++ database/migrations/2025_07_16_110021_create_guest_cart_items_table.php
@@ -6,28 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('guest_cart_items', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('guest_cart_id');
-            $table->unsignedBigInteger('product_id');
-            $table->unsignedInteger('quantity')->default(1);
-
-            $table->foreign('guest_cart_id')->references('id')->on('guest_carts')->onDelete('cascade');
-            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
-            // Ensure a product appears only once in a user's cart.
-            $table->unique(['guest_cart_id', 'product_id']);
+            $table->foreignId('guest_cart_id')->constrained('guest_carts')->onDelete('cascade');
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
+            $table->integer('quantity')->default(1);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('guest_cart_items');
